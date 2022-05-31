85f7ea5c082bd108ee388f0066a4a0dbed1629a1
Iswappyy <swadipthawkar0902@gmail.com>
2022-05-31 15:25:59 +0530

    First commit

---
+++ src/com/githubdemo/GitDemo1.java
@@ -0,0 +1,15 @@
+package com.githubdemo;
+
+public class GitDemo1 {
+public static void main(String[] args) {
+	int a = 10;
+	int b = 5;
+	
+	b = a + b;
+	a = b - a;
+	b = b - a;
+	
+	System.out.println("Value of a is: "+a);
+	System.out.println("Value of b is: "+b);
+
+}}
